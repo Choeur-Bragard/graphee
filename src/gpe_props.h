bf644ce65c4fc40a2e8e6b5a759b22d5ce339a82
NoelM <noel.martin.07@gmail.com>
2018-05-18 15:25:15 +0200

    slight adjustments

---
+++ src/gpe_props.h
@@ -0,0 +1,56 @@
+#ifndef GPE_PROPS_H
+#define GPE_PROPS_H
+
+#include <iostream>
+#include <string>
+
+namespace graphee {
+
+class gpe_props {
+public:
+  enum {B, KB, MB, GB};
+
+  gpe_props () {}
+
+  gpe_props (std::string in_name, uint64_t in_nvertices, uint64_t in_nslices, uint64_t in_nthreads, 
+      uint64_t in_ram_limit, int unitRL, uint64_t in_sort_limit, int unitSL) :
+
+    name(in_name), nvertices(in_nvertices), nslices(in_nslices),
+    nthreads(in_nthreads), ram_limit(in_ram_limit), sort_limit(in_sort_limit) {
+
+      if (unitRL == KB) {
+        ram_limit *= 1 << 10;
+      } else if (unitRL == MB) {
+        ram_limit *= 1 << 20;
+      } else if (unitRL == GB) {
+        ram_limit *= 1 << 30;
+      }
+
+      if (unitSL == KB) {
+        sort_limit *= 1 << 10;
+      } else if (unitSL == MB) {
+        sort_limit *= 1 << 20;
+      } else if (unitSL == GB) {
+        sort_limit *= 1 << 30;
+      }
+
+      nvertices *= 1000000;
+      window = nvertices/nslices;
+      nblocks = nslices*nslices;
+  }
+
+  ~gpe_props () {}
+
+  std::string name;
+  uint64_t nthreads;
+  uint64_t nvertices;
+  uint64_t ram_limit;
+  uint64_t sort_limit;
+  uint64_t nslices;
+  uint64_t nblocks;
+  uint64_t window;
+};
+
+} // namespace graphee
+
+#endif // SETINGS_H
