f2e6e2f1540e75683c8a84e500a81f892de83a3d
NoelM <noel.martin.07@gmail.com>
2018-05-31 17:35:46 +0200

    added graphee

---
+++ src/graphee.h
@@ -0,0 +1,15 @@
+#ifndef GRAPHEE_H
+#define GRAPHEE_H
+
+#include "gpe_utils.h"
+#include "gpe_props.h"
+
+#include "gpe_diskvec.h"
+#include "gpe_vec.h"
+
+#include "gpe_diskmat.h"
+#include "gpe_bsmat_csr.h"
+
+#include "gpe_pagerank.h"
+
+#endif // GRAPHEE_H
