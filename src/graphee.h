d65034c322835752dcfb84deac9bc4deebef47ba
NoelM <noel.martin.07@gmail.com>
2018-06-05 12:10:20 +0200

    added master header

---
+++ src/graphee.h
@@ -0,0 +1,10 @@
+#ifndef GRAPHEE_H
+#define GRAPHEE_H
+
+#include "properties.h"
+#include "utils.h"
+#include "sparse_matrix_csr.h"
+#include "disk_sparse_matrix.h"
+#include "vector.h"
+
+#endif // GRAPHEE_H
